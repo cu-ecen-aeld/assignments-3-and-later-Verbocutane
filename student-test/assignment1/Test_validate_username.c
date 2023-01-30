d84df645cdcd946f4054d6aeb32f0dea5a98806c
Verbocutane <s.laroche@pransystems.com>
2023-01-30 15:58:46 -0500

    - Add missing files to assignment.

--- student-test/assignment1/Test_validate_username.c
+++ student-test/assignment1/Test_validate_username.c
@@ -18,5 +18,9 @@ void test_validate_my_username()
      * TODO: Replace the line below with your code here as described above to verify your /conf/username.txt 
      * config file and my_username() functions are setup properly
      */
-    TEST_ASSERT_TRUE_MESSAGE(false,"AESD students, please fix me!");
+	const char *FromCfgFIle =	malloc_username_from_conf_file();
+	const char *HardCoded = my_username();
+
+	
+    TEST_ASSERT_TRUE_MESSAGE((FromCfgFIle != NULL) && (HardCoded != NULL) && (strcmp(FromCfgFIle, HardCoded) == 0),"AESD students, please fix me!");
 }
